eb95a63e8442d807a87a0b3932627764df25a3e0
María Simó <holasoymariasimo@gmail.com>
2024-01-03 14:00:19 +0100

    feat: add logs to reval route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -5,10 +5,12 @@ export async function POST(request: Request) {
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get("x-vercel-reval-key");
 
+  console.log("reval", secret);
   if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
+  console.log("revalitation");
   revalidateTag("custom");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
