8fffc95927308f82fd86dd9f409da99a49212e1c
María Simó <holasoymariasimo@gmail.com>
2024-01-10 19:38:45 +0100

    feat: add locale negotiation middleware

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,1 +1,14 @@
-type Locale = "en" | "es";
+import Negotiator from "negotiator";
+import { match } from "@formatjs/intl-localematcher";
+import { NextRequest } from "next/server";
+
+export const locales = ["en", "es"];
+export const defaultLocale = "en";
+
+export function getLocale(request: NextRequest) {
+  const languages = new Negotiator({
+    headers: Object.fromEntries(request.headers),
+  }).languages();
+
+  return match(languages, locales, defaultLocale);
+}
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import { getLocale, locales } from "./lib/i18n";
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  return Response.redirect(request.nextUrl);
+}
