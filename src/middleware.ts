1cd96cba4705ad6a0b06f8ccd3c9f213d7824fb0
María Simó <holasoymariasimo@gmail.com>
2024-01-11 00:34:35 +0100

    feat: hide default locale from url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest } from "next/server";
-import { getLocale, locales } from "./lib/i18n";
+import { NextRequest, NextResponse } from "next/server";
+import { defaultLocale, getLocale, locales } from "./lib/i18n";
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
@@ -7,10 +7,26 @@ export function middleware(request: NextRequest) {
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
-
-  if (pathnameHasLocale) return;
-
   const locale = getLocale(request);
+
+  if (pathnameHasLocale) {
+    return;
+  }
+
+  // hide default locale from url: instead of /en/my-page use /my-page
+  if (locale === defaultLocale) {
+    return NextResponse.rewrite(new URL(`${locale}${pathname}`, request.url));
+  }
+
   request.nextUrl.pathname = `/${locale}${pathname}`;
   return Response.redirect(request.nextUrl);
 }
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    "/src/app",
+  ],
+};
