40032478d6c29e0a5ff8339c2acf97921220bce1
María Simó <holasoymariasimo@gmail.com>
2024-01-11 11:03:26 +0100

    fix: fix getStaticParams for locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export async function generateStaticParams() {
   return locales.map((locale) => {
-    return { lang: locale };
+    return { locale };
   });
 }
 
